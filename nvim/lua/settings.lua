9798c45b7dd3477296e6f29bf325cd7d95760985
WinterSunset95 <wintersunset95@gmail.com>
2022-05-03 09:20:15 +0530

    major changes to the plugin system

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -6,7 +6,7 @@ local o = vim.opt
 o.number = true
 o.relativenumber = true
 o.cursorline = true
-o.mouse = a
+o.mouse = "a"
 o.smartindent = true
 o.autoindent = true
 o.scrolloff = 5
@@ -16,17 +16,17 @@ o.shiftwidth = 4
 g.tabwidth = 4
 o.softtabstop = 4
 o.wildmenu = true
-o.wildmode = longest
+o.wildmode = "longest,list"
 o.ttyfast = true
 o.clipboard = "unnamedplus"
 o.splitbelow = true
 o.termguicolors = true
 o.completeopt = "menuone,noselect,menu"
+o.laststatus=2
 
 -- Vim-Plug
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
-Plug('dracula/vim', {as = 'dracula'})
 Plug 'mattn/emmet-vim'
 Plug('neoclide/coc.nvim', {branch = 'release'})
 Plug 'nvim-lua/plenary.nvim'
@@ -34,19 +34,21 @@ Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'BurntSushi/ripgrep'
 Plug 'sharkdp/fd'
 Plug 'nvim-telescope/telescope.nvim'
-Plug 'preservim/nerdtree'
-Plug 'ryanoasis/vim-devicons'
 vim.call('plug#end')
 
 -- Leader keys
 g.mapleader = ","
 g.user_emmet_leader_key = ","
 
+-- Netrw configs
+g['netrw_liststyle'] = 3
+g['netrw_banner'] = 0
+g['netrw_browse_split'] = 4
+
 -- Stuff I couldn't quite figure out how to do with lua
 vim.cmd [[
 	set nowrap
-	colorscheme dracula
-	au VimEnter * :NERDTree
+	au VimEnter * :Vexplore
 	au VimEnter * :vertical resize 15
 	au VimEnter * :set winfixwidth
 	au VimEnter * :tabnew +term
